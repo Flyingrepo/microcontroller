7f4e5a83ab6dcdbd1ca421734f6ef0c495ad1eb2
Flyingrepo <162626258+Flyingrepo@users.noreply.github.com>
2024-07-27 10:39:24 +0530

    Update case.c

--- case.c
+++ case.c
@@ -1,12 +1,12 @@
-#include <string.h>
+
 int main(){
 int i;
-char a[8]="StUdEnTs";
-for(i=0;i<strlen(a);i++){
-	if(a[i]>='A'&& a[i]<='Z'){
+char a[8]={'S','t','U','D','e','T','D'};
+for(i=0;i<7;i++){
+	if(a[i]>=97){
 		a[i]=a[i]-32;
 	}
-	else if(a[i]>='a'&& a[i]<='z'){
+	else {
 		a[i]=a[i]+32;
 	}
 }
